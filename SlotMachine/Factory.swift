dc3c7e55104db27d8e704af5b3c9cee52f96e1a0
otamm <oivatom@gmail.com>
2015-06-12 12:55:54 -0300

    Added factory class

---
+++ SlotMachine/Factory.swift
@@ -0,0 +1,10 @@
+//
+//  Factory.swift
+//  SlotMachine
+//
+//  Created by Otavio Monteagudo on 6/12/15.
+//  Copyright (c) 2015 Otavio Monteagudo. All rights reserved.
+//
+
+import Foundation;
+import UIKit;
